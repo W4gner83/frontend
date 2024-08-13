e976911fd584acc6bcc9757c8c99971e937a6474
W4gner83 <wagner.rrodriguez@gmail.com>
2024-08-13 10:39:20 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld { // Aqui é criada a classe HelloWorld
+    public static void main (String [] args) { // Aqui é criado o método executor main
+        System.out.println("Hello World!!!"); // Aqui é "impressa" uma linha com o texto "Hello World"
+    }  // Aqui encerra o bloco de código do método main
+}  // Aqui encerra o bloco de código da classe HelloWorld
