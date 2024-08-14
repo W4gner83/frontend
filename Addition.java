fb7d7285840a0840613e59375fcbdcaf01afb75c
W4gner83 <wagner.rrodriguez@gmail.com>
2024-08-14 11:52:12 -0300

    Tradução de texto para o português

--- Addition.java
+++ Addition.java
@@ -8,21 +8,21 @@ public class Addition // declaração da classe, méódo ou variável
    {
       // obtain user input from JOptionPane input dialogs
       String firstNumber = // String é a classe de texto, FirstNumber é a variável
-         JOptionPane.showInputDialog("Enter first integer"); // JOptionPane é a classe para a criação da caixa de diálogo e o ponto invoca o showInputDialog que exibe a caixa solicitada
+         JOptionPane.showInputDialog("Digite o primeiro número"); // JOptionPane é a classe para a criação da caixa de diálogo e o ponto invoca o showInputDialog que exibe a caixa solicitada
       String secondNumber =
-         JOptionPane.showInputDialog("Enter second integer"); // JOptionPane é a classe para a criação da caixa de diálogo e o ponto invoca o showInputDialog que e
+         JOptionPane.showInputDialog("Digite o segundo número"); // JOptionPane é a classe para a criação da caixa de diálogo e o ponto invoca o showInputDialog que e
 
       // convert String inputs to int values for use in a calculation
       int number1 = Integer.parseInt(firstNumber); // int é o tipo, number1 a variável, o sinal de igual uma operação, o Integer classe de número inteiro, o ponto invoca a função e parseInt a função de conversão de string para número inteiro 
-      int number2 = Integer.parseInt(secondNumber); int é o tipo, number1 a variável, o sinal de igual uma operação, o Integer classe de número inteiro, o ponto invoca a função e parseInt a função de conversão de string para número inteiro 
+      int number2 = Integer.parseInt(secondNumber); //int é o tipo, number1 a variável, o sinal de igual uma operação, o Integer classe de número inteiro, o ponto invoca a função e parseInt a função de conversão de string para número inteiro 
 
       int sum = number1 + number2; // add numbers // int é o tipo, sum o método, igual é o operador de atribuição, number 1 e number 2 as variáveis
 
       // display result in a JOptionPane message dialog
-      JOptionPane.showMessageDialog(null, "The sum is " + sum, //JOptionPane é a classe para a criação da caixa de diálogo e o ponto invoca o showInputDialog 
+      JOptionPane.showMessageDialog(null, "A soma dos números é" + sum, //JOptionPane é a classe para a criação da caixa de diálogo e o ponto invoca o showInputDialog 
       //que exibe a caixa solicitada, a frase entre aspas o texto (string), o sinal de + neste caso um contatenador e a vígula um separador
       
-         "Sum of Two Integers", JOptionPane.PLAIN_MESSAGE); // frase entre aspas o string, JOptionPane é a classe para a criação da caixa de diálogo, o ponto para invocar o PLAIN_MESSAGE (screamcase)
+         "Soma dos dois inteiros", JOptionPane.PLAIN_MESSAGE); // frase entre aspas o string, JOptionPane é a classe para a criação da caixa de diálogo, o ponto para invocar o PLAIN_MESSAGE (screamcase)
    } 
 } // end class Addition
 
